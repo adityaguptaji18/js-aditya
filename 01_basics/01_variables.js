ceef7ecb8df3ccf8f803842ca2cfa64dc3509764
adityaguptaji18 <ag2500310@gmail.com>
2024-10-01 19:39:44 +0000

    variables and constant

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=31456
+let accountEmail="aditya@abes.in"
+var accountPass="125467"
+accountCity="Noida"
+console.table([accountId , accountEmail ,accountPass , accountCity])
+// accountId=12345
+accountEmail="gupta@"
+accountPass="00001"
+accountCity="Gurgaon"
+let accountState;
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
